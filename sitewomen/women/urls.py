cf53743226c8e1c98ab79e8a703f1e35e1925f25
grvsa <grvsa1980@gmail.com>
2024-09-09 15:52:25 +0300

    Lesson 1.6 Dynamic URLS

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 ##from women.views import index, categories
 from . import views ## удобнее так
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('cats/', views.categories),
+##    path('cats/', views.categories),
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
     path('', views.index),
+#    re_path(r'^archive/(?P<year>[0-9]{4})/',views.archive),
+    path('archive/<year4: year>/', views.archive)
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -8,5 +8,16 @@ def index(request):
     return HttpResponse("Women app index page")
 
 
-def categories(request):
-    return HttpResponse('<h1>Categories page</h1>')
+##def categories(request):
+##    return HttpResponse('<h1>Categories page</h1>')
+
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Categories page</h1><p>id: {cat_id}</p>')
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Categories page</h1><p>slug: {cat_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Categories page</h1><p>archive year: {year}</p>')
